ca3ca7dd1bc9cd285433dcb05718af06338af21f
ilyasshanavazi <ilyas.shanavazi@city.ac.uk>
2026-03-22 14:45:08 +0000

    slight change

--- programmers/Source_Code_Demo/src/IPOS_Detailed_Design/Main.java
+++ programmers/Source_Code_Demo/src/IPOS_Detailed_Design/Main.java
@@ -2,6 +2,6 @@ package IPOS_Detailed_Design;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Project is working!");
+        System.out.println("Testing if the project is working!");
     }
 }
