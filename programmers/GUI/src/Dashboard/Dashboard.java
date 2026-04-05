aa350b401b7b65bc793f670b4da10773d395d438
Kaitlyn <kaitlyn.pilar@city.ac.uk>
2026-04-05 19:54:29 +0100

    Dashboard small edit

--- programmers/GUI/src/Dashboard/Dashboard.java
+++ programmers/GUI/src/Dashboard/Dashboard.java
@@ -417,7 +417,7 @@ public class Dashboard extends javax.swing.JFrame {
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class, java.lang.Boolean.class, java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class
+                java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class, java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class
             };
 
             public Class getColumnClass(int columnIndex) {
